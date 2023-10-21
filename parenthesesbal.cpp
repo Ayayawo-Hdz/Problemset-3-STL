a58071fdbe08f3ebd0e179aed985d9b4ebe79bca
Yahir Hernández <148612192+Ayayawo-Hdz@users.noreply.github.com>
2023-10-21 01:29:41 -0600

    Create parenthesesbal.cpp
    3
    ([])
    (([()])))
    ([()[]()])()

---
+++ parenthesesbal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    bool temp;
+    string par;
+    vector<string> resp;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        getline(cin, par);
+        stack<char> dip;
+        for (int i = 0; i < par.size(); i++) {
+            if (par[i] == '[' || par[i] == '(') {
+                dip.push(par[i]);
+            } else if (!dip.empty()) {
+                if ((dip.top() == '[' && par[i] == ']') || (dip.top() == '(' && par[i] == ')')) {
+                    dip.pop();
+                } else {
+                    dip.push(par[i]);
+                }
+            } else {dip.push(par[i]);}
+        }
+        if (dip.empty()) {temp = true;} else {temp = false;}
+        if (temp) {resp.push_back("Yes");} else {resp.push_back("No");}
+    }
+    for (int i = 0; i < resp.size(); i++) {cout << resp[i] << endl;}
+
+    return 0;
+}
