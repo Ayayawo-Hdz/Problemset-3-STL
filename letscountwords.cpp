9e919612868ef65d5458dbfcac56307ecb4d9502
Yahir Hernández <148612192+Ayayawo-Hdz@users.noreply.github.com>
2023-10-21 01:31:46 -0600

    Create letscountwords.cpp
    7
    word icpc rdwo dwor ordw pcic ccpi

---
+++ letscountwords.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+void doSome();
+
+using namespace std;
+
+void doSome() {
+    int n, x = 0;
+    
+    cin >> n;
+    string w;
+    map<string, bool> words;
+    
+    while (n--) {
+        cin >> w;
+        if (n == 0) {cin.ignore(1, ' ');}
+        bool exists = false;
+        for (const auto& pair : words) {
+            const string& word = pair.first;
+            for (int i = 0; i < word.length(); ++i) {
+                string rotated = word.substr(i) + word.substr(0, i);
+                if (rotated == w) {exists = true; break;}
+            }
+            if (exists) {break;}
+        }
+        if (!exists) {words[w] = true; x++;}
+    }
+    cout << x << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    doSome();
+    
+    return 0;
+}
