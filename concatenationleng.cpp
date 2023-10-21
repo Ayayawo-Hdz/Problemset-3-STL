1e1273bd35e10b9452f989ee3866cab5f98b7ac6
Yahir Hernández <148612192+Ayayawo-Hdz@users.noreply.github.com>
2023-10-21 01:31:08 -0600

    Create concatenationleng.cpp
    2
    3 2
    cat
    dog
    mouse
    rat
    bat
    1 1
    abc
    cab

---
+++ concatenationleng.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+void doSome();
+
+using namespace std;
+#define endl '\n'
+
+void doSome() {
+    int t, c = 1;
+    
+    cin >> t;
+    cin.ignore();
+    while (t--){
+        int m, n, com = 0;
+        map<string, int> words;
+        
+        cin >> m >> n;
+        cin.ignore();
+        string st1[m], st2[n];
+        
+        for (int i = 0; i < m; i++) {getline(cin, st1[i]);}
+        for (int i = 0; i < n; i++) {getline(cin, st2[i]);}
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {words[st1[j] + st2[k]]++;}
+        }
+        cout << "Case " << c << ": " << words.size() << endl; c++;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    doSome();
+    
+    return 0;
+}
