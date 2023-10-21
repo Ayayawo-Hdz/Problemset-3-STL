1f0994b5bcd83a831814d562c98ced0d1c459b95
Yahir Hernández <148612192+Ayayawo-Hdz@users.noreply.github.com>
2023-10-21 01:27:42 -0600

    Create triple.cpp
    7
    1
    1
    3
    2 2 2
    7
    2 2 3 3 4 2 2
    8
    1 4 3 4 3 2 4 1
    9
    1 1 1 2 2 2 3 3 3
    5
    1 5 2 4 3
    4
    4 4 4 4

---
+++ triple.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define endl '\n';
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int rep, counter, n, e, repi;
+    vector<int> elements;
+
+    cin >> rep;
+
+    while (rep--) {
+        bool selec = false;
+        elements.clear();
+        counter = 0;
+        repi = 0;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> e;
+            elements.push_back(e);
+            if (i != n - 1) {
+                cin.ignore(1, ' ');
+            }
+        }
+        sort(elements.begin(), elements.end());
+
+        for (int i = 0; i < elements.size() - 1; i++) {
+            if (elements[i] == elements[i + 1]) {
+                counter++;
+                if (counter >= 2) {
+                    selec = true;
+                    repi = elements[i];
+                }
+            } else {
+                counter = 0;
+            }
+        }
+        if (selec) {
+            cout << repi << endl;
+        } else {
+            cout << "-1" << endl;
+        }
+    }
+    return 0;
+}
